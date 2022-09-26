4c8deb4a5bfd123b31d11fbe0a73cfa1ac2d084e
ciberseguranca <hugoteixeira@enta.pt>
2022-09-26 12:10:35 +0000

    ACABAR EM CASA 2 E fazer o resto

--- Ex2.py
+++ Ex2.py
@@ -9,3 +9,19 @@ Após o utilizador ter inderido a frase apresente:
  - Quantos numeros tem a frase
  - Apresente a frase invertida. Exemplo: A frase é 'Bom dia!' deve dar '!aid moB'
 """
+
+
+if __name__ == '__main__':
+    vogais_count = 0
+    vogais = set('aeiouAEIOU')
+    continuar = 's'
+    while continuar == 's':
+        frase = str(input('Insira uma frase: '))
+        nl = frase.split(' ')
+        print(f'A frase tem {len(nl)} palavras')
+        for char in frase:
+            if char in vogais:
+                vogais_count += 1
+        print(f'A frase tem {vogais_count} vogais')
+        continuar = input('Repetir [s | n] ? ')
+    print(f'Adeus!')
