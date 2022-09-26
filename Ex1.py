32e8bd9a0906497efae3f9b071a237596dbeb19f
hugot <hugoteixera@enta.pt>
2022-09-26 20:35:26 +0000

    Exerc CASA

--- Ex1.py
+++ Ex1.py
@@ -8,3 +8,22 @@ Após o utilizador ter inserido os 5 valores apresente:
 - O maior valor inserido
 - A média das vendas
 """
+
+if __name__ == '__main__':
+  vendas_grupo_central = []
+  nome_ilhas_grupo_central = []
+  valores = 5
+
+  for x in range(0, valores):
+    num_vendas = int(input(f'Qual é o {x + 1}º número de vendas? '))
+    nome_ilha = str(input(f'Qual é a ilha do grupo central? '))
+
+    vendas_grupo_central.insert(x, num_vendas)
+    nome_ilhas_grupo_central.insert(x, nome_ilha)
+
+  print(f'\nVendas - {vendas_grupo_central}')
+  print(f'Ilhas - {nome_ilhas_grupo_central}')
+  print(f'Total de vendas é {sum(vendas_grupo_central)}')
+  print(f'Menor vendas foi {min(vendas_grupo_central)}')
+  print(f'Maior vendas foi {max(vendas_grupo_central)}')
+  print(f'Média de vendas foi {sum(vendas_grupo_central) / len(vendas_grupo_central)}')
--- Ex2.py
+++ Ex2.py
@@ -11,17 +11,33 @@ Após o utilizador ter inderido a frase apresente:
 """
 
 
+import re
 if __name__ == '__main__':
-    vogais_count = 0
-    vogais = set('aeiouAEIOU')
-    continuar = 's'
-    while continuar == 's':
-        frase = str(input('Insira uma frase: '))
-        nl = frase.split(' ')
-        print(f'A frase tem {len(nl)} palavras')
-        for char in frase:
-            if char in vogais:
-                vogais_count = vogais_count + 1
-        print(f'A frase tem {vogais_count} vogais')
-        continuar = input('Repetir [s | n] ? ')
-    print(f'Adeus!')
+    frase_list = []
+    frase = str(input('Insira uma frase: '))
+
+
+    num_palavras = len(frase.split(' '))
+    num_mai = 0
+    num_min = 0
+    num_vogais = 0
+    for x in range(len(frase)):
+        if (frase[x].islower()):
+            num_min += 1
+        elif (frase[x].isupper()):
+            num_mai += 1
+        if frase[x] == 'a' or frase[x] == 'e' or frase[x] == 'i' or frase[x] == 'o' or frase[x] == 'u' or frase[x] == 'A' or frase[x] == 'E' or frase[x] == 'I' or frase[x] == 'O' or frase[x] == 'U':
+            num_vogais += 1
+
+    num_letras = (num_mai + num_min)
+
+    print(f'A é frase escrita foi "{frase_list}"')
+    print(f'A frase contém {num_palavras} palavras')
+    print(f'A frase contém {num_letras} letras')
+    print(f'A frase contém {num_vogais} vogais')
+    print(f'A frase contém {num_mai} letras maiusculas')
+    print(f'A frase contém {num_min} letras minusculas')
+    reversed_frase = ''
+    for i in reversed(frase):
+        reversed_frase += i
+    print(f'A é frase escrita quanto invertida fica "{reversed_frase}"')
